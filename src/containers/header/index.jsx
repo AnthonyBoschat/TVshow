e4d453b9e2eabe697786d2461a0426313671e122
AnthonyBoschat <AnthonyBoschat.dev@hotmail.com>
2025-06-13 12:41:33 +0200

    fix(import)

--- src/containers/header/index.jsx
+++ src/containers/header/index.jsx
@@ -1,6 +1,6 @@
 import s from "./style.module.scss";
 import logo from "../../core/assets/logo.png"
-import { ROUTES } from "../../core/routes";
+import { ROUTES } from "../../core/routes.js";
 import { SearchInput } from "./components/searchInput";
 import { useNavigate } from "react-router";
 import { useRef } from "react";
